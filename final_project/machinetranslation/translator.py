b86abddcc001bce7faed5d99d8cfa94b9a2d2980
Amine-Mahmoud <youremail@domain.com>
2022-01-26 23:16:41 +0000

    Final changes

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,7 +1,9 @@
-import json
+""" This is the translation module"""
+
+import os
+#import json
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-import os
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -17,18 +19,22 @@ language_translator = LanguageTranslatorV3(
 
 language_translator.set_service_url(url)
 
-def englishToFrench(englishText):
+def english_to_french(english_text):
     """Translate an english text to french"""
-    translation = language_translator.translate(
-    text = englishText,
-    model_id = 'en-fr').get_result()
-    frenchText = translation["translations"][0]["translation"]
-    return frenchText
+    french_text = ''
+    if english_text != '':
+        translation = language_translator.translate(
+        text = english_text,
+        model_id = 'en-fr').get_result()
+        french_text = translation["translations"][0]["translation"]
+    return french_text
 
-def frenchToEnglish(frenchText):
+def french_to_english(french_text):
     """Translate a french text to english"""
-    translation = language_translator.translate(
-    text = frenchText,
-    model_id = 'fr-en').get_result()
-    englishText = translation["translations"][0]["translation"]
-    return englishText
+    english_text = ''
+    if french_text != '':
+        translation = language_translator.translate(
+        text = french_text,
+        model_id = 'fr-en').get_result()
+        english_text = translation["translations"][0]["translation"]
+    return english_text
